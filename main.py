0779d80e2710d0561c0381bf324609a1cb62936d
PavelLyndia <servicefox17@gmail.com>
2023-09-11 16:57:32 +0300

    Fix some issues

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ def is_valid_digits(input_string):
 print("End add commit")
 print("end")
 
+print("HELLO")
+
+print("Python")
+
+
+
+
 # Примеры использования:
 print(is_valid_digits("1928346"))    # True
 print(is_valid_digits("123Д02349"))  # False
